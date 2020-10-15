ecf0ea72e4e55d7fd94fe20d6520a195eb0b1079
pauline-mfr <p.monfrance@codeur.online>
2020-10-15 16:55:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <title>Aurora</title>
+  </head>
+  <body>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-2">
+      SEARCH BAR
+    </div>
+    <div class="offset-8 col-2">
+      BURGER MENU
+    </div>
+  </div>
+</div><!--  END NAVBAR -->
+
+<div class="col-4 mx-auto">
+  <img src="logo.png" alt="logo Aurora Project">
+</div>
+
+
+<div class="container py-5" id="video">     <!-- VIDEO HEADER -->
+<h2>Lorem Ipsum Dolor elet<br> magnus rosarum?</h2>
+<button class="btn">Button text ></button>
+<div id="circle">
+<div id="play"><a href="#"></a></div>
+</div>
+</div>
+
+
+<div class="container" id="articles">
+
+<div class="row">
+  <div class="col-6">
+    <img src="img.jpg" alt="" >
+  </div>
+  <div class="col-6">
+    <div class="line"></div>
+    <h3>Lorem ipsum doloret amet</h3>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <a href="#">> Read more</a>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-6">
+    <div class="line"></div>
+    <h3>Lorem ipsum doloret amet</h3>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <a href="#">> Read more</a>
+  </div>
+  <div class="col-6">
+    <img src="img.jpg" alt="" >
+  </div>
+</div>
+
+<!-- PARTIE DE LISA -->
+
+<div class="row">
+  <div class="col-6">
+    <img src="img.jpg" alt="" >
+  </div>
+  <div class="col-6">
+    <div class="line"></div>
+    <h3>Lorem ipsum doloret amet</h3>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <a href="#">> Read more</a>
+  </div>
+</div>
+</div>
+
+
+<!--  END ARTICLES -->
+
+
+<div class="container" id="missions">
+<div class="row">
+  <h2>Missions</h2>
+  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+</div>
+<div class="row">
+  <div class="col-4">
+    <img src="tel.png">
+    <h4>Mission 1</h4>
+    <p>Lorem Ipsum is simply dummy text</p>
+    <button class="btn">> Learn more</button>
+    <div class="underline"></div>
+  </div>
+  <div class="col-4">
+    <img src="tel.png">
+    <h4>Mission 2</h4>
+    <p>Lorem Ipsum is simply dummy text</p>
+    <button class="btn">> Learn more</button>
+    <div class="underline"></div>
+  </div>
+  <div class="col-4">
+    <img src="tel.png">
+    <h4>Mission 3</h4>
+    <p>Lorem Ipsum is simply dummy text</p>
+    <button class="btn">> Learn more</button>
+    <div class="underline"></div>
+  </div>
+</div>
+</div>
+
+
+<footer>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js" integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous"></script>
+
+  </body>
+</html>
