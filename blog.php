6968c84a26a47ee497b2b5d2662356523d89e53a
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-10-16 11:51:05 +0200

    create blog page

---
+++ blog.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <!-- FONTAWESOME -->
+    <script src="https://kit.fontawesome.com/5bf2af5d34.js" crossorigin="anonymous"></script>
+
+    <title>Aurora</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+        <div class="mt-2 d-flex justify-content-between">
+            <div>
+                <i class="fas fa-search fa-3x color-icons"></i>
+            </div>
+            <div>
+                <img src="logo.png" alt="logo Aurora Project">
+            </div>
+            <div>
+                <i class="fas fa-bars fa-3x color-icons"></i>
+            </div>
+        </div>
+    </div>
+
+    <!-- ARTICLES -->
+    <div class="container-fluid mt-5">
+        <h2 class="color-purple border-bottom-title-articles">Articles récents</h2>
+
+        <h3 class="text-uppercase color-purple">Titre de l'article</h3>
+
+        <div class="mt-4 d-flex justify-content-center rounded bg-vert-eau">
+            <div>
+                <img src="images/femmes.jpg" alt="femmes" class="img-width-articles">
+            </div>
+            <div class="d-flex flex-column">
+                <h4 class="text-uppercase color-purple">Catégorie de l'article</h4>
+                <p>Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. Ille quidem fructum omnis ante actae vitae hodierno die maximum cepit, cum summo consensu senatus, tum iudicio tuo gravissimo et maximo. Ex quo profecto intellegis quanta in dato beneficio sit laus, cum in accepto sit tanta gloria.</p>
+                <a href="#" class="color-purple"> > Lire la suite</a>
+            </div>
+            
+        </div>
+    </div>
+
+
+<footer class="mt-5">
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js" integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous"></script>
+
+  </body>
+</html>
