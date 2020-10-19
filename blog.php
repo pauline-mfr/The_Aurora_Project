d6e97a22a343cc9805711125ab062145d070e426
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-10-19 16:38:42 +0200

    add pagination to blog page

--- blog.php
+++ blog.php
@@ -77,6 +77,20 @@
         </div>
     </div>
 
+    <hr>
+    <!-- PAGINATION -->
+    <div class="pagination d-flex justify-content-center mt-5 mb-5">
+        <a href="#">&laquo;</a>
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+        <a href="#">6</a>
+        <a href="#">&raquo;</a>
+    </div>
+
+
 
 
     
