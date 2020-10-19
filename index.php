c96f0e21052940cbd400b94f6febe482f0ca1958
pauline-mfr <p.monfrance@codeur.online>
2020-10-19 16:40:25 +0200

    Mobile version

--- index.php
+++ index.php
@@ -4,35 +4,35 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css" type="text/css">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/5bf2af5d34.js" crossorigin="anonymous"></script>
     <title>Aurora</title>
   </head>
   <body>
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-2">
-
-    </div>
-    <div class="offset-8 col-2">
-      
-    </div>
-  </div>
-</div><!--  END NAVBAR -->
-
-
-<div class="col-4 mx-auto">
-  <img src="logo.png" alt="logo Aurora Project">
-</div>
+    <div class="container-fluid">
+           <div class="mt-2 d-flex justify-content-between">
+               <div>
+                   <i class="fas fa-search fa-3x color-icons"></i>
+               </div>
+               <div>
+                   <img src="logo.png" alt="logo Aurora Project">
+               </div>
+               <div>
+                   <i class="fas fa-bars fa-3x color-icons"></i>
+               </div>
+           </div>
+       </div><!--  END NAVBAR -->
 
 
 <div class="container py-5" id="video">     <!-- VIDEO HEADER -->
 <div class="row">
-  <div class="col-4 relative">
+  <div class="col-md-4 relative">
 <h2 class="relative">Lorem Ipsum Dolor elet<br> magnus rosarum?</h2>
 <h2 id="video-mobile">Lorem Ipsum Dolor elet<br> magnus rosarum?</h2>
 <button class="btn relative">Button text ></button>
 <div id="left-border"></div>
 </div>
-<div class="col-8 d-flex justify-content-center align-items-center pt-5">
+<div class="col-12 col-md-8 d-flex justify-content-center align-items-center pt-5">
 <div id="circle">
 <div id="play"><a href="#"></a></div>
 </div>
@@ -44,10 +44,10 @@
 <div class="container" id="articles">
 
 <div class="row">
-  <div class="col-6">
+  <div class="col-12 col-md-6">
     <img src="img.jpg" alt="" >
   </div>
-  <div class="col-6">
+  <div class="col-12 col-md-6">
     <div class="line"></div>
     <h3>Lorem ipsum doloret amet</h3>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
@@ -56,13 +56,13 @@
 </div>
 
 <div class="row">
-  <div class="col-6">
+  <div class="col-12 col-md-6">
     <div class="line"></div>
     <h3>Lorem ipsum doloret amet</h3>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
     <a href="#">Read more</a>
   </div>
-  <div class="col-6">
+  <div class="col-12 col-md-6">
     <img src="img.jpg" alt="" >
   </div>
 </div>
@@ -70,10 +70,10 @@
 <!-- PARTIE DE LISA -->
 
 <div class="row">
-  <div class="col-6">
+  <div class="col-12 col-md-6">
     <img src="img.jpg" alt="" >
   </div>
-  <div class="col-6">
+  <div class="col-12 col-6">
     <div class="line"></div>
     <h3>Lorem ipsum doloret amet</h3>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
@@ -92,21 +92,21 @@
   <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
 </div>
 <div class="row">
-  <div class="col-4">
+  <div class="col-sm-12 col-md-4">
     <img src="tel.png">
     <h4>Mission 1</h4>
     <p>Lorem Ipsum is simply dummy text</p>
     <button class="btn">Learn more</button>
     <div class="underline"></div>
   </div>
-  <div class="col-4">
+  <div class="col-sm-12 col-md-4">
     <img src="tel.png">
     <h4>Mission 2</h4>
     <p>Lorem Ipsum is simply dummy text</p>
     <button class="btn">Learn more</button>
     <div class="underline"></div>
   </div>
-  <div class="col-4">
+  <div class="col-sm-12 col-md-4">
     <img src="tel.png">
     <h4>Mission 3</h4>
     <p>Lorem Ipsum is simply dummy text</p>
@@ -118,6 +118,23 @@
 
 
 <footer>
+  <div class="row">
+<div class="col-md-4">
+  <img src="logo.png" alt="logo Aurora">
+</div>
+<div class="col-md-4">
+<ul>
+  <li>A propos</li>
+  <li>Contact</li>
+  <li>Articles</li>
+</ul>
+</div>
+<div class="col-md-4">
+SOCIAL MEDIA
+</div>
+  </div>
+  <div class="row">
+  </div>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
