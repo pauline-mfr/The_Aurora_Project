b4eee8057365e84dc029d237ffa8ac9ff6376d20
pauline-mfr <p.monfrance@codeur.online>
2020-10-20 16:49:09 +0200

    wordpress functions and insert

--- header.php
+++ header.php
@@ -2,14 +2,30 @@
 <html <?php language_attributes(); ?> >
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <title><?php wp_title ?></title>
+  <script src="https://kit.fontawesome.com/5bf2af5d34.js" crossorigin="anonymous"></script>
+  <title><?php wp_title() ?></title>
   <?php wp_head() ?>
 </head>
 <body <?php body_class(); ?> >
   <?php wp_body_open(); ?>
+
+
   <header class="header">
-    <div class="container">
-      <p class="color-icons">blablala</p>
+    <div class="container-fluid">
+           <div class="mt-2 d-flex justify-content-between">
+               <div>
+                   <i class="fas fa-search fa-3x color-icons"></i>
+               </div>
+               <div>
+                 <?php if (function_exists('the_custom_logo')) {
+                   the_custom_logo();
+                 } ?>
+               </div>
+               <div>
+                   <i class="fas fa-bars fa-3x color-icons"></i>
+               </div>
+           </div>
+       </div>
     </header>
 
 
