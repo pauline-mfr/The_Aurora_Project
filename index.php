b5c1c9a98a25af3e537e4d1e7bbfaacb128c5979
pauline-mfr <p.monfrance@codeur.online>
2020-10-20 09:51:41 +0200

    WP theme

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
-    <link rel="stylesheet" href="main.css" type="text/css">
+    <link rel="stylesheet" href="style.css" type="text/css">
     <meta name=viewport content="width=device-width, initial-scale=1">
     <script src="https://kit.fontawesome.com/5bf2af5d34.js" crossorigin="anonymous"></script>
     <title>Aurora</title>
