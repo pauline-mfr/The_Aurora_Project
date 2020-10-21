67591acc1b18a61efcd2505684690483ffa0e2fd
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-10-21 11:56:08 +0200

    add post pagination wordpress function

--- home.php
+++ home.php
@@ -33,10 +33,13 @@ if (have_posts()) :
     <?php the_post_thumbnail('original', ['class' =>'img-articles border-radius', 'alt' => '']); ?>
     </div>
     <div class="border-radius bg-vert-eau d-flex flex-column position-text pt-5">
-        <div class="position-categorie text-center">
-            <h4 class="text-uppercase color-purple"><?php the_category(); ?></h4>
-            <p class="mt-5 pl-5 pr-5 pb-4"><?php echo wp_trim_words( get_the_content(), 40, '...' ); ?></p>
-            <a href="<?php the_permalink() ?>" class="color-purple text-uppercase border-lire-la-suite"> > Lire la suite</a>
+        <div class="position-categorie mt-4 text-center">
+            <?php $categories = get_the_category();
+            foreach ($categories as $category){
+            echo '<div class="mb-5"> <a class="text-uppercase color-purple" href="' . get_category_link($category->term_id) . '" target="_blank">' . $category->cat_name . '</a> </div>';
+            }?>
+            <p class="mt-4 pl-5 pr-5 pb-4"><?php echo wp_trim_words( get_the_content(), 40, '...' ); ?></p>
+            <a href="<?php the_permalink() ?>" target="_blank" class="color-purple text-uppercase border-lire-la-suite"> > Lire la suite</a>
         </div>
     </div>
 </div>
@@ -54,15 +57,14 @@ endif;
 
 <hr>
 <!-- PAGINATION -->
-<div class="pagination d-flex justify-content-center mt-5 mb-5">
-    <a href="#">&laquo;</a>
-    <a href="#">1</a>
-    <a href="#">2</a>
-    <a href="#">3</a>
-    <a href="#">4</a>
-    <a href="#">5</a>
-    <a href="#">6</a>
-    <a href="#">&raquo;</a>
+
+<div class="d-flex justify-content-center mt-5 mb-5">
+    <?php the_posts_pagination(array(
+            'mid_size' => 2,
+            'prev_text' => __( '<i class="fas fa-angle-double-left"></i>', 'textdomain' ),
+            'next_text' => __( '<i class="fas fa-angle-double-right"></i>', 'textdomain'), 
+            'screen_reader_text' => ' ',
+    ) ); ?>
 </div>
 
 
