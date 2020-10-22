22e397d313709b2d4378ff8b8bae313be4a3af72
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-10-22 16:41:53 +0200

    html input search

---
+++ searchform.php
@@ -0,0 +1,3 @@
+<form action="<?php bloginfo('home'); ?>" autocomplete="on" class="rechercher">
+    <input id="s" name="s" type="text" placeholder="Vous recherchez ?"><input id="search_submit" value="Rechercher" type="submit">
+</form>
