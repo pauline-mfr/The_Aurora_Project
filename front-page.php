9a15150349aedcc66f2bc0559e65c57dd2fa30d5
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-10-22 12:21:32 +0200

    [doing] trying to display accordion

--- front-page.php
+++ front-page.php
@@ -1,21 +1,5 @@
 <?php get_header()?>
 
-<div id="css-script-menu">
-  <div class="css-script-center">
-    <div class="css-script-ads"><script type="text/javascript"><!--
-google_ad_client = "ca-pub-2783044520727903";
-/* CSSScript Demo Page */
-google_ad_slot = "3025259193";
-google_ad_width = 728;
-google_ad_height = 90;
-//-->
-</script>
-      <script type="text/javascript"
-src="//pagead2.googlesyndication.com/pagead/show_ads.js">
-</script></div>
-    <div class="css-script-clear"></div>
-  </div>
-</div>
 
 <h1>Les dernières infographies</h1>
 
@@ -26,30 +10,20 @@ src="//pagead2.googlesyndication.com/pagead/show_ads.js">
     ) );
 ?>
 
-<?php while (have_posts()) : the_post(); ?>
 <div class="container div-accordion">
 <div class="accordion">
 		<ul>
+    <?php while (have_posts()) : the_post(); ?>
 			<li>
-                <?php the_post_thumbnail("original");?>
-				<a href="<?php the_permalink(); ?>"> Lien </a>
+        <?php the_post_thumbnail("original");?>
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a>
 			</li>
 		</ul>
 	</div>
-
 </div>
 
 <?php endwhile; ?>
 
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-46156385-1', 'cssscript.com');
-  ga('send', 'pageview');
-</script>
 
 
 <?php get_footer()?>
