c5b31acfa138446f3d85443ab2af70267f8ffa02
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-10-22 09:09:51 +0200

    [doing] carousel

--- index.php
+++ index.php
@@ -59,6 +59,87 @@
 </div>
 
 <!-- PARTIE DE LISA -->
+<div id="css-script-menu">
+  <div class="css-script-center">
+    <div class="css-script-ads"><script type="text/javascript"><!--
+google_ad_client = "ca-pub-2783044520727903";
+/* CSSScript Demo Page */
+google_ad_slot = "3025259193";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+      <script type="text/javascript"
+src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></div>
+    <div class="css-script-clear"></div>
+  </div>
+</div>
+<h1>Les dernières infographies</h1>
+<div class="accordion">
+  <ul>
+    <li>
+    <img src="images/soleil-couchant-femme-voiture-tomber.jpg" alt="" class="cell">
+      <div> <a href="#">
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </a> </div>
+    </li>
+    <li>
+    <img src="images/soleil-couchant-femme-voiture-tomber.jpg" alt="" class="cell">
+      <div> <a href="#">
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </a> </div>
+    </li>
+    <li>
+    <img src="images/soleil-couchant-femme-voiture-tomber.jpg" alt="" class="cell">
+      <div> <a href="#">
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </a> </div>
+    </li>
+    <li>
+    <img src="images/soleil-couchant-femme-voiture-tomber.jpg" alt="" class="cell">
+      <div> <a href="#">
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </a> </div>
+    </li>
+    <li>
+    <img src="images/soleil-couchant-femme-voiture-tomber.jpg" alt="" class="cell">
+      <div> <a href="#">
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </a> </div>
+    </li>
+    <li>
+    <img src="images/soleil-couchant-femme-voiture-tomber.jpg" alt="" class="cell">
+      <div> <a href="#">
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </a> </div>
+    </li>
+  </ul>
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46156385-1', 'cssscript.com');
+  ga('send', 'pageview');
+
+</script>
+
+
+<h2 class="mt-5">Accordion 2</h2>
+
+
+
+
+<!-- FIN - LISA -->
 
 <div class="row">
   <div class="col-6">
