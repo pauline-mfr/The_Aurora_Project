33912cbceaba723084594861829d46ee0706e406
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-10-23 11:57:18 +0200

    add bootstrap class to search form

--- searchform.php
+++ searchform.php
@@ -1,3 +1,4 @@
-<form action="<?php bloginfo('home'); ?>" autocomplete="on" class="rechercher">
-    <input id="s" name="s" type="text" placeholder="Vous recherchez ?"><input id="search_submit" value="Rechercher" type="submit">
+<form action="<?php bloginfo('home'); ?>" autocomplete="on" class="rechercher d-flex flex-row mr-3">
+    <input id="s" name="s" type="text"  class="form-control">
+    <input id="search_submit" value="Rechercher" type="submit" class="btn btn-sm btn-dark">
 </form>
