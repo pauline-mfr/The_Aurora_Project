bd8b515ba09a150f1a4da9ab69ec26cb2b148adb
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-10-26 16:51:28 +0100

    add color title

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
             <?php the_post_thumbnail('original', ['class' =>'img-fluid', 'alt' => '']); ?>
         </div>
         <div class="div-width-article mx-auto pt-4 pb-4 pr-4 pl-4 mb-5 border rounded bg-white border-shadow">
-            <h2 class="text-uppercase text-center"><?php the_title(); ?></h2>
+            <h2 class="text-uppercase color-icons text-center"><?php the_title(); ?></h2>
             <p class="mb-5"><?php the_content(); ?></p>
         </div>
     </div>
